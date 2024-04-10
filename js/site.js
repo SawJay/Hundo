5b4f97e7da7be4cdc894b05a66325d88ea5daaf3
SawJay <sawyerjd1@gmail.com>
2024-04-10 14:25:17 -0400

    Finished JS for CountUP

--- js/site.js
+++ js/site.js
@@ -1,9 +1,7 @@
 // Collect user inputs
-// Aka entry point
 function getValues() {
 // get the <input> element for startNumber and grab its value
-let startInputElement = document.getElementById('startNumber');
-let startNumber = startInputElement.value;
+let startNumber = document.getElementById('startNumber').value;
 
 // do the same for endNumber
 let endNumber = document.getElementById('endNumber').value;
@@ -11,18 +9,35 @@ let endNumber = document.getElementById('endNumber').value;
 startNumber = Number(startNumber)
 endNumber = Number(endNumber)
 
-let generatedNumbers = generateValues(startNumber, endNumber);
+if (isNaN(startNumber) || isNaN(endNumber)) {
+  Swal.fire ({
+    icon: 'error',
+    title: 'Oops!',
+    text: 'Please enter valid numbers for CountUP! to use.',
+    backdrop: false
+  })
+} else if ((startNumber > endNumber) || (startNumber < 0) || (endNumber > 100)) {
+  Swal.fire ({
+    icon: 'error',
+    title: 'Oops!',
+    text: 'Please enter valid numbers for CountUP! to use.',
+    backdrop: false
+})
+
+} else {let generatedNumbers = generateValues(startNumber, endNumber);
 
 displayValues (generatedNumbers);
+}
 
 }
+
 
 // Generate a list of numbers between those two values
 
 function generateValues (start, end) {
   let numbers = [];
   
-  for (let i = start; i <= end; i = i + 1) {
+  for (let i = start; i <= end; i++) {
     numbers.push(i);
   }
 
@@ -37,11 +52,19 @@ function displayValues (numberArray) {
         // index  0  1  2  3  4  5
 let tableHtml = '';
 
-for (let index = 0; index < numberArray.length; index = index + 1) {
+for (let index = 0; index < numberArray.length; index +=1) {
 
    let number = numberArray[index];
+
+   let className = '';
+
+   if (number % 2 == 0) {
+    className = 'even';
+   } else {
+    className = 'odd';
+   }
    
-   tableHtml = tableHtml + '<tr><td>' + number + '</tr></td>'
+   tableHtml += `<tr><td class="${className}">${number}</td></tr>`;
 
 }
 
